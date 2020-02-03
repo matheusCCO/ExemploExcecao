633c07b2553800226bd37218e0c200335443d919
Matheus Bandeira <matheus.bandeira@gruporbs.com.br>
2020-02-03 19:34:33 -0300

    Versão ruim

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,31 +17,33 @@ public class Program {
 		
 		System.out.print("Room number: ");
 		int number = sc.nextInt();
-		System.out.print("Check-in (dd/MM/yyyy)");
+		System.out.print("Check-in (dd/MM/yyyy):");
 		Date checkIn = sdf.parse(sc.next());
-		System.out.print("Check-out (dd/MM/yyyy)");
+		System.out.print("Check-out (dd/MM/yyyy):");
 		Date checkOut = sdf.parse(sc.next());
 		
 		if (!checkOut.after(checkIn)) {
-			System.out.println("Error in reservation: Reservation dates for update must be future dates");
+			System.out.println("Error in reservation: Check-out date must be after check-in date.");
 		} else {
 			Reservation res = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation: "+res);
 			
 			System.out.println();
 			System.out.println("Enter data to update the reservation");
-			System.out.print("Check-in (dd/MM/yyyy)");
+			System.out.print("Check-in (dd/MM/yyyy):");
 			checkIn = sdf.parse(sc.next());
-			System.out.println("Check-out (dd/MM/yyyy)");
+			System.out.print("Check-out (dd/MM/yyyy):");
 			checkOut = sdf.parse(sc.next());
-			res.updateDates(checkIn, checkOut); 
+	
+			String error = res.updateDates(checkIn, checkOut); 	
+			if(error != null) {
+				System.out.println("Error in reservation:" + error);
+			} else {
+				System.out.println("Reservation: "+res);
+			}
 			
-			
-			System.out.println("Reservation: "+res);
 		}
 		
-		 
-
 	}
 
 }
